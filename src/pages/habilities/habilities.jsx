93930417cade478718d80078ad7808bd1bc88458
Denilson Salas G <denilsonsalguz@gmail.com>
2025-03-12 17:33:32 -0500

    uploading cv

--- src/pages/habilities/habilities.jsx
+++ src/pages/habilities/habilities.jsx
@@ -4,20 +4,23 @@ import html from '../../assets/img/html.png'
 import css from '../../assets/img/css.png'
 import js from '../../assets/img/js.png'
 import react from '../../assets/img/react.png'
-import mysql from '../../assets/img/mysql.png'
+import mysql from '../../assets/img/mysql.svg'
 import node from '../../assets/img/node.png'
+import git from '../../assets/img/git.svg'
+import bootstrap from '../../assets/img/bootstrap.svg'
 export function Habilities(){
     return(
         <Container>
             <div className='habilitiesContainer'>
                 <div className='habilitiesSubcontainer'>
-                    <h2>Herramientas</h2>
+                    <h2>Herramientas Frontend</h2>
                     <hr className='line'/>
                     <div className='containerTools'>
                         <img loading='lazy' className='imgTools' src={html} alt="html" />
                         <img loading='lazy' className='imgTools' src={css}  alt="css" />
                         <img loading='lazy' className='imgTools' src={js}  alt="js" />
                         <img loading='lazy' className='imgTools' src={react}  alt="react" />
+                        <img loading='lazy' className='imgTools' src={bootstrap}  alt="bootstrap" />
                     </div>
                 </div>
                 <div className='habilitiesSubcontainer'>
@@ -31,11 +34,12 @@ export function Habilities(){
                     </ul>
                 </div>
                 <div className='habilitiesSubcontainer'>
-                    <h2>Herramientas en proceso de Aprendizaje</h2>
+                    <h2>Otras Herramientas</h2>
                     <hr className='line'/>
                     <div className='containerTools'>
                         <img loading='lazy' className='imgTools' src={mysql} alt="mysql" />
                         <img loading='lazy' className='imgTools' src={node} alt="nodejs" />
+                        <img loading='lazy' className='imgTools' src={git} alt="git" />
                     </div>
                 </div>
             </div>
